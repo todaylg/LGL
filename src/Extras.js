d11c259d4b71f020e352c8b521f3acae769465ae
todaylg <lg2lugang@gmail.com>
2019-01-24 23:16:21 +0800

    add:MSDF Text

--- src/Extras.js
+++ src/Extras.js
@@ -3,3 +3,4 @@ export {Orbit} from './extras/Orbit.js';
 export {Plane} from './extras/Plane.js';
 export {Cube} from './extras/Cube.js';
 export {Sphere} from './extras/Sphere.js';
+export {Text} from './extras/Text.js';
