779fea0d9f833157d728a41d4621e831fc021216
todaylg <lg2lugang@gmail.com>
2019-01-28 00:28:14 +0800

    add:looper test

--- src/math/Color.js
+++ src/math/Color.js
@@ -48,4 +48,25 @@ export class Color extends Float32Array {
             parseInt(r[3], 16) / 255
         ] : null;
     }
+
+    setHSL(h, s, l) {
+        var r, g, b;
+        if (s == 0) {
+            r = g = b = l; // achromatic
+        } else {
+            let hue2rgb = function hue2rgb(p, q, t) {
+                if (t < 0) t += 1;
+                if (t > 1) t -= 1;
+                if (t < 1 / 6) return p + (q - p) * 6 * t;
+                if (t < 1 / 2) return q;
+                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+                return p;
+            }
+            let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            let p = 2 * l - q;
+            this.r = hue2rgb(p, q, h + 1 / 3);
+            this.g = hue2rgb(p, q, h);
+            this.b = hue2rgb(p, q, h - 1 / 3);
+        }
+    }
 }
