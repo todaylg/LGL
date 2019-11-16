cdc26b8676ce1e593ccb45a85c33d30e21cda5a4
todaylg <lg2lugang@gmail.com>
2019-11-17 00:35:43 +0800

    Update:Petal

--- examples/shaders/PetalShader.js
+++ examples/shaders/PetalShader.js
@@ -42,7 +42,7 @@ in vec2 vUv;
 
 out vec4 FragColor;
 void main(void) {
-    FragColor.rgb = 0.5 + 0.3 * sin(vUv.yxx + uTime) + vec3(0.2, 0.0, 0.1);
+    FragColor.rgb = 0.7 + 0.1 * sin(vUv.yxx + uTime) + vec3(0.2, 0.0, 0.1);
     FragColor.a = 1.0;
 }
 `;
